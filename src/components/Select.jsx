891065ede21a29b27e7beb64579b4c122bac38be
samdish jadhav <samdishjadhav9970@gmail.com>
2023-11-10 23:05:03 +0530

    Production grade components

---
+++ src/components/Select.jsx
@@ -0,0 +1,24 @@
+import React, { useId } from 'react'
+
+function Select({
+    options,
+    label,
+    className='',
+    ...props
+}, ref) {
+    const id = useId()
+  return (
+    <div className='w-full' >
+        {label && <label htmlFor={id} className='' ></label>}
+        <select {...props} className={`duration-200 border border-gray-200 w-full px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 ${className} `} name="" id={id} ref={ref} >
+            {options?.map((option)=>(
+                <option key={option} value={option} >
+                    {option}
+                </option>
+            ))}
+        </select>
+    </div>
+  )
+}
+
+export default React.forwardRef(Select)
