933205e9ff6ee28746170526d830d28515198c73
samdish jadhav <samdishjadhav9970@gmail.com>
2023-11-15 10:39:37 +0530

    form and slug values

---
+++ src/components/RTE.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react'
+import { Controller } from 'react-hook-form'
+
+export default function RTE() {
+  return (
+    <div>RTE</div>
+  )
+}
